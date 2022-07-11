a439fd1cee45ffea233167f029569f28b14d5098
Natalya Botyasova <mrrnyash@gmail.com>
2022-07-11 18:58:04 +0800

    Added quickstart

--- my_strategy.py
+++ my_strategy.py
@@ -1,6 +1,9 @@
 from model.game import Game
 from model.order import Order
+from model.unit_order import UnitOrder
 from model.constants import Constants
+from model.vec2 import Vec2
+from model.action_order import ActionOrder
 from typing import Optional
 from debug_interface import DebugInterface
 
@@ -10,7 +13,15 @@ class MyStrategy:
         pass
 
     def get_order(self, game: Game, debug_interface: Optional[DebugInterface]) -> Order:
-        return Order({})
+        orders = {}
+        for unit in game.units:
+            if unit.player_id != game.my_id:
+                continue
+            orders[unit.id] = UnitOrder(
+                Vec2(-unit.position.x, -unit.position.y),
+                Vec2(-unit.direction.y, unit.direction.x),
+                ActionOrder.Aim(True))
+        return Order(orders)
 
     def debug_update(self, displayed_tick: int, debug_interface: DebugInterface):
         pass
